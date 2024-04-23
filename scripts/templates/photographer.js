767a2f2268b97181f1b32d0e0252264b8ff2f21d
piirk <baptiste.boucharechas@gmail.com>
2024-04-23 02:35:05 +0200

    recup photographers

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,39 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
-
+    const { name, portrait, country, city, tagline, price } = data;
     const picture = `assets/photographers/${portrait}`;
-
+    
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = `<article><a href="photographer.html" alt="${name}²"><img src="${picture}" alt="" /><h2>${name}</h2></a><h3>${city}, ${country}</h3><p>${tagline}</p><span>${price}€/jour</span></article>`;
+        console.log(article)
+        return article;
+
+        /*
+        const article = document.createElement('article');
+        const a = document.createElement('a');
+        a.setAttribute('href', 'photographer.html');
+        a.setAttribute('alt', name) ;
+
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const h3 = document.createElement('h3');
+        h3.textContent = `${city}, ${country}`;
+        const p = document.createElement('p');
+        p.textContent = tagline;
+        const span = document.createElement('span');
+        span.textContent = `${price}€/jour`;
+
+        a.appendChild(img);
+        a.appendChild(h2);
+
+        article.appendChild(a);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(span);
+
         return (article);
+        */
     }
     return { name, picture, getUserCardDOM }
 }
