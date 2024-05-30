785cebebdad478f7185934551fd6b4dc9b694722
piirk <baptiste.boucharechas@gmail.com>
2024-05-30 17:25:14 +0200

    PhotographerApp init

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -13,6 +13,19 @@ class PhotographerApp {
         return this._medias
     }
 
+    init() {
+        this.generateProfile()
+        this.generateSnippet()
+        this.generateMedias()
+
+        document.querySelector("title").textContent = `Fisheye - ${this.photographer.name}`
+        document.querySelector(".modal__header__title").innerHTML = `Contactez-moi<br />${this.photographer.name}`
+
+        this.addListenersSort()
+        this.addListenersLikes()
+        this.addListenersKeyboard()
+    }
+
     sortMedias(sortBy) {
         let sortedMedias = [];
         
@@ -34,12 +47,83 @@ class PhotographerApp {
         return sortedMedias;
     }
 
-    generateMedias(sortBy = "popularité") {
-        const sortedMedias = this.sortMedias(sortBy)
-        const mediasTemplate = new MediasTemplate(sortedMedias)
+    generateProfile() {
+        new PhotographerTemplate(this.photographer).createProfile()
+    }
 
+    generateSnippet() {
+        new SnippetTemplate(this.photographer, this.medias).createSnippet()
+    }
+
+    generateMedias(sortBy = "popularité") {
+        const mediasTemplate = new MediasTemplate(this.sortMedias(sortBy))
         mediasTemplate.createPhotographerMedias()
         mediasTemplate.createLightBoxMedias()
+    }
+
+    // listeners pour le formulaire de tri
+    addListenersSort() {
+        document.querySelector('.medias-form').addEventListener('change', () => {
+            this.generateMedias(document.getElementById('order-select').value)
+            this.addListenersLikes()
+        })
+    }
+
+    // listeners pour les likes
+    addListenersLikes() {
+        document.querySelectorAll('.card__likes').forEach(button => {
+            button.addEventListener('click', function() {
+                // incrémentation sur button + snippet
+                const likesCountButton = this.querySelector('.likes-count')
+                const likesCountSnippet = document.querySelector('.likes-count-snippet')
+                const heartIcon = this.querySelector('i')
+
+                let likes = parseInt(likesCountButton.textContent)
+                let likesTotal = parseInt(likesCountSnippet.textContent)
+
+                if (heartIcon.classList.contains('fa-solid')) {
+                    likes -= 1
+                    likesTotal -= 1
+                    this.setAttribute('aria-pressed', 'false')
+                } else {
+                    likes += 1
+                    likesTotal += 1
+                    this.setAttribute('aria-pressed', 'true')
+                }
+
+                likesCountButton.textContent = likes
+                likesCountSnippet.textContent = likesTotal
+        
+                // changement icone
+                heartIcon.classList.toggle('fa-regular')
+                heartIcon.classList.toggle('fa-solid')
+            })
+        })
+    }
+
+    // listeners pour le clavier
+    addListenersKeyboard() {
+        document.addEventListener('keydown', function(e) {
+            // lightbox: escape, left arrow, right arrow
+            const lightBox = document.getElementById("lightbox_modal")
+            
+            if (lightBox.getAttribute('aria-hidden') == 'false' && e.key === "Escape") {
+                closeLightBox()
+            }
+            if (lightBox.getAttribute('aria-hidden') == 'false' && e.key === "ArrowLeft") {
+                document.querySelector(".lightbox__prev").click()
+            }
+            if (lightBox.getAttribute('aria-hidden') == 'false' && e.key === "ArrowRight") {
+                document.querySelector(".lightbox__next").click()
+            }
+    
+            // contact form: escape
+            const modal = document.getElementById('contact_modal')
+        
+            if (modal.getAttribute('aria-hidden') == 'false' && e.key === 'Escape') {
+                closeModal()
+            }
+        })
     }
 }
 
@@ -52,97 +136,9 @@ api.getPhotographerByIdWithMedias(photographerId).then(data => {
     let { photographer, medias } = data
     
     const app = new PhotographerApp(photographer, medias)
-
-    const photographerTemplate = new PhotographerTemplate(app.photographer)
-    const snippetTemplate = new SnippetTemplate(app.photographer, app.medias)
-
-    photographerTemplate.createProfile()
-    snippetTemplate.createSnippet()
-
-    app.generateMedias()
+    app.init()
 
     // var pour lightbox
     let openingSlide = {}
     let slideIndex = 1
-
-    //
-    document.querySelector("title").textContent = `Fisheye - ${photographer.name}`
-    document.querySelector(".modal__header__title").innerHTML = `Contactez-moi<br />${photographer.name}`
-
-    // ajout des events listeners
-    addListeners(app)
-    addListenersLikes()
-    addListenersKeyboard()
 })
-
-// fct créer les events listeners
-function addListeners(app) {
-    // eventListener pour le form de tri
-    const sortForm = document.querySelector('.medias-form')
-    const sortBySelect = document.getElementById('order-select')
-
-    sortForm.addEventListener('change', (event) => {
-        const sortBy = sortBySelect.value
-        app.generateMedias(sortBy)
-        addListenersLikes()
-    })
-}
-
-// fct gestion des likes des médias
-function addListenersLikes() {
-    document.querySelectorAll('.card__likes').forEach(button => {
-        button.addEventListener('click', function() {
-            // incrémentation sur button + snippet
-            const likesCountButton = this.querySelector('.likes-count')
-            const likesCountSnippet = document.querySelector('.likes-count-snippet')
-            const heartIcon = this.querySelector('i')
-
-            let likes = parseInt(likesCountButton.textContent)
-            let likesTotal = parseInt(likesCountSnippet.textContent)
-
-            const isLiked = heartIcon.classList.contains('fa-solid')
-
-            if (isLiked) {
-                likes -= 1
-                likesTotal -= 1
-                this.setAttribute('aria-pressed', 'false')
-            } else {
-                likes += 1
-                likesTotal += 1
-                this.setAttribute('aria-pressed', 'true')
-            }
-
-            likesCountButton.textContent = likes
-            likesCountSnippet.textContent = likesTotal
-    
-            // changement icone
-            heartIcon.classList.toggle('fa-regular')
-            heartIcon.classList.toggle('fa-solid')
-        });
-    });
-}
-
-// fct créer listeners keyboard shortcuts
-function addListenersKeyboard() {
-    document.addEventListener('keydown', function(e) {
-        // lightbox: escape, left arrow, right arrow
-        const lightBox = document.getElementById("lightbox_modal")
-        
-        if (lightBox.getAttribute('aria-hidden') == 'false' && e.key === "Escape") {
-            closeLightBox()
-        }
-        if (lightBox.getAttribute('aria-hidden') == 'false' && e.key === "ArrowLeft") {
-            document.querySelector(".lightbox__prev").click()
-        }
-        if (lightBox.getAttribute('aria-hidden') == 'false' && e.key === "ArrowRight") {
-            document.querySelector(".lightbox__next").click()
-        }
-
-        // contact form: escape
-        const modal = document.getElementById('contact_modal')
-    
-        if (modal.getAttribute('aria-hidden') == 'false' && e.key === 'Escape') {
-            closeModal()
-        }
-    })
-}
